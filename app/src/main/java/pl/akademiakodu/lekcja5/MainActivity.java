3b406981a35bcc6c1e02cfbeaa07b874a64d0a45
Vallerboy <10135886>
2016-09-12 18:27:56 +0200

    First update.

--- app/src/main/java/pl/akademiakodu/lekcja5/MainActivity.java
+++ app/src/main/java/pl/akademiakodu/lekcja5/MainActivity.java
@@ -9,14 +9,13 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-     
+
     }
 
 
---
+++ app/src/main/java/pl/akademiakodu/lekcja5/OwnTextView.java
@@ -0,0 +1,18 @@
+package pl.akademiakodu.lekcja5;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.widget.TextView;
+
+/**
+ * Created by O on 2016-09-12.
+ */
+public class OwnTextView extends TextView {
+    public OwnTextView(Context context) {
+        super(context);
+
+        Typeface typeface = Typeface.createFromAsset(context.getAssets(), "fonts/arundinab.ttf");
+        setTypeface(typeface);
+
+    }
+}

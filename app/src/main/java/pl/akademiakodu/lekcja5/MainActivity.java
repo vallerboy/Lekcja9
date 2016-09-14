226e2df79bf35c349380ee6b2b2497d48189102d
Vallerboy <10135886>
2016-09-14 19:00:59 +0200

    FirstCommit

--- app/src/main/java/pl/akademiakodu/lekcja5/MainActivity.java
+++ app/src/main/java/pl/akademiakodu/lekcja5/MainActivity.java
@@ -25,6 +25,7 @@ import android.view.View;
 import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -41,6 +42,9 @@ import butterknife.OnItemSelected;
 
 public class MainActivity extends Activity {
 
+
+    @BindView(R.id.button2)
+    public Button button;
 
 
 
@@ -64,6 +68,12 @@ public class MainActivity extends Activity {
     }
 
 
+    @OnClick(R.id.button3)
+    public void onClick1(View v) {
+        createNotyficationBig();
+    }
+
+
     private void createNotyfication(){
         Intent intent = new Intent(this, Main2Activity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
@@ -76,13 +86,48 @@ public class MainActivity extends Activity {
                 .setSmallIcon(R.mipmap.ic_launcher)
                 //.setLargeIcon(R.mipmap.ic_launcher)
                 .setAutoCancel(false)
+
                 .setContentIntent(pendingIntent).build();
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationManager.notify(0, notification);
 
+        Log.e("noti", "działa!");
+
     }
-}
+
+
+    private void createNotyficationBig() {
+        Intent intent = new Intent(this, Main2Activity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        String[] strings = new String[3];
+        strings[0] = "Coś nowego";
+        strings[1] = "Coś starego";
+        strings[2] = "AkademiaKodu.pl";
+
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        inboxStyle.setBigContentTitle("Wiadomości: ");
+
+        for (int i = 0; i < strings.length; i++) {
+            inboxStyle.addLine(strings[i]);
+        }
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("Hello World!")
+                .setContentText("Witaj AkademioKodu, przybywam!")
+                .setTicker("Przyszła wiadomość...")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(false)
+                .setStyle(inboxStyle)
+                .setContentIntent(pendingIntent).build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
+
+
+    }
+
+    }
 
 
 
